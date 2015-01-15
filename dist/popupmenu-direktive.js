a235984044ff8f9e08a8f1a2be9041c3397abce5
Gil Desmarais <gd@thepeaklab.com>
2015-01-15 13:42:28 +0100

    release(): Release version 0.1.0

--- dist/popupmenu-direktive.js
+++ dist/popupmenu-direktive.js
@@ -8,7 +8,7 @@
  * offsetHorizontal: int (px)
  * width: int (px)
  * placement: 'none' | 'sw' | 'nw' | 'n' | 's'
- * toggle: 'hover' | 'both'
+ * toggle: 'hover' | 'click' | 'both'
  * closeTimeout: int (milliseconds)
  * offsetVertical: int (px)
  * triangle: true | false
@@ -26,15 +26,18 @@
  */
 angular.module('tpl.popupmenu', []).directive('popupmenu', [
   '$timeout',
-  function popupmenu($timeout) {
+  '$window',
+  function popupmenu($timeout, $window) {
     'use strict';
+    var $triangle = document.createElement('div');
+    $triangle.className = 'popupmenu__triangle';
     var DEFAULT_OPTIONS = {
         offsetHorizontal: 30,
         width: 200,
         placement: 'none',
         toggle: 'hover',
         closeTimeout: 300
-      }, POPUPMENU_TIMEOUT = 'popupMenuTimeout', $triangle = $('<div class="popupmenu__triangle" />');
+      }, POPUPMENU_TIMEOUT = 'popupMenuTimeout';
     var generateCss = function (attrs) {
       attrs.offsetHorizontal = attrs.offsetHorizontal || 0;
       attrs.offsetVertical = attrs.offsetVertical || 0;
@@ -67,21 +70,24 @@ angular.module('tpl.popupmenu', []).directive('popupmenu', [
       return basePositioning;
     };
     var togglePopupmenu = function ($content, css, options) {
-      if ($content.css('display') !== 'block') {
+      if ($window.getComputedStyle($content[0], null).getPropertyValue('display') !== 'block') {
         showPopupmenu($content, css);
       } else {
         hidePopupmenu($content, options.closeTimeout);
       }
     };
     var showPopupmenu = function ($content, css) {
-      if ($content.data(POPUPMENU_TIMEOUT)) {
-        $timeout.cancel($content.data(POPUPMENU_TIMEOUT));
+      if ($content[0].getAttribute('data-' + POPUPMENU_TIMEOUT)) {
+        $timeout.cancel($content[0].getAttribute('data-' + POPUPMENU_TIMEOUT));
       }
-      $content.css('display', 'block').css(css);
+      for (var cssKey in css) {
+        $content[0].style[cssKey] = css[cssKey];
+      }
+      $content[0].style.display = 'block';
     };
     var hidePopupmenu = function ($content, closeTimeout) {
-      $content.data(POPUPMENU_TIMEOUT, $timeout(function () {
-        $content.css({ 'display': 'none' });
+      $content[0].setAttribute('data-' + POPUPMENU_TIMEOUT, $timeout(function () {
+        $content[0].style.display = 'none';
       }, closeTimeout));
     };
     return {
@@ -89,7 +95,7 @@ angular.module('tpl.popupmenu', []).directive('popupmenu', [
       transclude: true,
       template: '<div class="popupmenu" ng-transclude></div>',
       link: function (scope, element, attrs) {
-        var $content = $('.popupmenu__content:first', element);
+        var $content = element[0].getElementsByClassName('popupmenu__content');
         // create options
         attrs = angular.extend(DEFAULT_OPTIONS, attrs);
         // add triangle and add the given placement string as element-modifier
@@ -99,7 +105,7 @@ angular.module('tpl.popupmenu', []).directive('popupmenu', [
           $content.append(triangleClone);
         }
         // generate css for this directive and hold in scope
-        var contentCss = generateCss(attrs, $('.popupmenu__trigger', element));
+        var contentCss = generateCss(attrs, element[0].getElementsByClassName('popupmenu__trigger'));
         // bind toggle events
         if (attrs.toggle === 'hover' || attrs.toggle === 'both') {
           element.on('mouseover', function () {
